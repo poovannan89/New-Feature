e2268a2d6e50448532a56782484f276170869d5c
Chithra <jayshoonla@gmail.com>
2021-02-15 17:39:47 +0530

    trying out webhook

--- FirstProgram.java
+++ FirstProgram.java
@@ -1,5 +1,6 @@
 public class FirstProgram {
   public static void main(String[] args){
     System.out.println("This is my first program in java");
+    System.out.println("Trying the webhook concept");
   }//End of main
 }//End of FirstJavaProgram Class
