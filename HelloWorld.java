ed8b74a8d0bba321a783c4aa056123a207d38ffb
poovannan <poovannancse@gmail.com>
2021-02-17 16:40:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Chithra!");
 	System.out.println("Hello, World!");
+	System.out.println("Testing Poll SCM!");
 
     }
 }
